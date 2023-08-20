e034017cd904944a6ff003e4fb2152e469c2ab3e
Nika Oniani <nikaoniani2018@gmail.com>
2023-08-20 17:42:20 +0400

    successful api call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+import './App.css'
+
 const App = () => {
+  const [info, setInfo] = useState()
+
+
   const fetchHandler = async () => {
+    // brandenburg gate, germany
+
+    let latitude = 52.516266;
+    let longitude = 13.377775;
+    let apiKey = "bb3d2d98459078abb132df015a9e175f";
     await fetch(
-      "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=bb3d2d98459078abb132df015a9e175f"
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
     )
       .then((response) => response.json())
-      .then((responseData) => console.log(responseData));
+      .then((responseData) => {
+        setInfo(responseData)
+        console.log(responseData)
+      });
   };
 
   return (
     <div>
+      <h3>Country: {info.sys['country']}</h3>
       <button onClick={fetchHandler}>Fetch</button>
-      {/* <label htmlFor="state">State: </label>
-      <input id="state" type="text" /> */}
     </div>
   );
 };
